c4999c5dc0092a4009a5216db28e46067c605c3e
catatsea <abc666182008@hotmail.com>
2015-12-15 10:14:59 +0800

    创建项目

---
+++ sinablogWithOC/sinablogWithOC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sinablogWithOC
+//
+//  Created by ZhangZiang on 15/12/15.
+//  Copyright © 2015年 ZhangZiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sinablogWithOC/sinablogWithOC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sinablogWithOC
+//
+//  Created by ZhangZiang on 15/12/15.
+//  Copyright © 2015年 ZhangZiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
